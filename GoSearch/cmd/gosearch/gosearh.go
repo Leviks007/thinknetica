05626a07f0b95130fb91d053a82b766b5060ebd4
Lev <leviks007@yandex.ru>
2023-06-28 21:02:45 +0700

    H/w 2

--- GoSearch/cmd/gosearch/gosearh.go
+++ GoSearch/cmd/gosearch/gosearh.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"lesson2/GoSearch/pkg/crawler"
+	"lesson2/GoSearch/pkg/crawler/spider"
+	"log"
+	"strings"
+)
+
+func main() {
+
+	searchKeyword := flag.String("s", "", "Параметр поиска")
+	flag.Parse()
+	urls := getURLs()
+	documents := scanWebsites(urls)
+
+	if *searchKeyword != "" {
+		printMatchingURLs(documents, *searchKeyword)
+	}
+}
+
+func getURLs() []string {
+	return []string{"https://go.dev", "https://golang.org"}
+}
+
+func scanWebsites(urls []string) []crawler.Document {
+	var documents []crawler.Document
+	s := spider.New()
+
+	for _, url := range urls {
+		doc, err := s.Scan(url, 2)
+		if err != nil {
+			log.Printf("Ошибка при сканировании сайта %s: %v", url, err)
+			continue
+		}
+		documents = append(documents, doc...)
+	}
+
+	return documents
+}
+
+func printMatchingURLs(documents []crawler.Document, keyword string) {
+	for _, doc := range documents {
+		if strings.Contains(doc.Title, keyword) {
+			fmt.Println("URL:", doc.URL)
+		}
+	}
+}
